f672b66f7ce209798f1f56537e61a453a233953f
wojtek534 <wojtek.rzepkaa@gmail.com>
2018-02-10 11:15:40 +0100

    0.33

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import './style/css/style.css';
 import App from './components/app/App';
 import registerServiceWorker from './registerServiceWorker';
+import './setWindow';
 
 // Redux
 import configureStore from './redux/store/configureStore';
